3918b16ab428fa59c87c7132641e297cd7fc241c
arisa <arisankochira10@gmail.com>
2018-11-21 18:36:52 +0900

    B2よくわからないけどPrintでくくっただけでも表示された

--- customer.py
+++ customer.py
@@ -1,7 +1,8 @@
-class Customer:  #B1
-    def __init__(self, first_name, family_name):
+class Customer:  # B1 #A2
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
+        self.age = age
 
     def name(self):
         return self.first_name + self.family_name
@@ -9,10 +10,18 @@ class Customer:  #B1
     def full_name(self):
         print(self.name())
 
+        print(self.age)
+
 
 if __name__ == "__main__":
-    ken = Customer("Ken ", "Takahashi")
+    ken = Customer("Ken ", "Takahashi", 15)
     ken.full_name()
+    ken.age
 
-    tom = Customer("Tom ", "Ford")
+    tom = Customer("Tom ", "Ford", 57)
     tom.full_name()
+    tom.age
+
+    ieyasu = Customer("Ieyasu ", "Tokugawa", 73)
+    ieyasu.full_name()
+    ieyasu.age
